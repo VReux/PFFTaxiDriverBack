1e3691e349c50b1b84a856ea55a14dcb2e7d4c0c
LKerdraon <lydiekerdraon@gmail.com>
2023-06-14 14:57:50 +0200

    correction securité

--- src/main/java/com/inti/config/SecurityConfig.java
+++ src/main/java/com/inti/config/SecurityConfig.java
@@ -1,14 +1,25 @@
 package com.inti.config;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import com.inti.services.impl.AppUserDetailsService;
+
+
 
 @Configuration // Pour créer un Bean dans le context Spring avec le nom securityConfig
 @EnableWebSecurity // Activation de la sécurité Web
@@ -24,18 +35,38 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		auth.userDetailsService(appUserDetailsService).passwordEncoder(bCryptPasswordEncoder);
 	}
 
-	public void configure(HttpSecurity http) throws Exception {
-		http // la sécurité des requetes http
-				.authorizeRequests().anyRequest().authenticated() // vérifier les requetes envoyées par les utilisateurs
-																	// :
-																	// http://localhost:9090/utilisateurs
-																	// http://localhost:9090/login
-				.and().formLogin() // formulaire login fournit par le projet Spring Security
-				.permitAll().and().logout().logoutUrl("/logout").permitAll().and().httpBasic().and().csrf().disable(); // méthode
-																														// d'attaque
-																														// informatique
-																														// (cybersécurité)
+	 //Activation : Formulaire d'authentification du projet Spring Security + la méthode BAA
+		public void configure(HttpSecurity http) throws Exception{
+			http 
+				.authorizeRequests()
+					.anyRequest().permitAll()
+				.and()
+				.formLogin() // formulaire login fournit par le projet Spring Security
+					.permitAll()
+				.and()
+				.logout()
+					.logoutUrl("/logout")
+					.permitAll()
+				.and()
+				.httpBasic() // BAA
+				.and()
+				.csrf().disable(); // méthode d'attaque informatique (cybersécurité)
+		}
+		@Bean
+		public FilterRegistrationBean simpleCorsFilter() {
+			UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+			CorsConfiguration config = new CorsConfiguration();
 
-	}
+			config.setAllowCredentials(true);
+			config.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+			config.setAllowedMethods(Collections.singletonList("*")); // GET, POST, PUT, DELETE, PATCH
+			config.setAllowedHeaders(Collections.singletonList("*"));
+
+			source.registerCorsConfiguration("/**", config);
+
+			FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+			bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+			return bean;
+		}
 
 }
