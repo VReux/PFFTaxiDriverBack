e6ef6e87e2d615b1b55e045543c13f601ccdcaeb
“HBienenfeld” <helene.bienenfeld@gmail.com>
2023-06-20 00:44:52 +0200

    je crois que je n'ai rien modifié

--- src/main/java/com/inti/repositories/AgenceRepository.java
+++ src/main/java/com/inti/repositories/AgenceRepository.java
@@ -13,4 +13,5 @@ public interface AgenceRepository extends JpaRepository<Agence, Long> {
 
 	
 	Agence findByNomAgence(String nomAgence);
+	
 }
--- src/main/java/com/inti/services/impl/AgenceService.java
+++ src/main/java/com/inti/services/impl/AgenceService.java
@@ -40,10 +40,7 @@ public class AgenceService implements IAgenceService {
 
 	@Override
 	public Agence findByNomAgence(String nomAgence) {
-		// TODO Auto-generated method stub
 		return agenceRepository.findByNomAgence(nomAgence);
 	}
-
-
-
+	
 }
